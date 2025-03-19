6062186c92a3f773519a3257bad7b35242221007
mahshidmoulaei <m.mmoolaei@yahoo.com>
2025-03-19 02:16:44 -0700

    Config data lay code app

---
+++ config.py
@@ -0,0 +1,9 @@
+# Embeddings Model
+MODEL_NAME = "sentence-transformers/all-MiniLM-L6-v2"
+
+# Text Chunking Parameters
+CHUNK_SIZE = 1000
+CHUNK_OVERLAP = 100
+
+# Retrieval Settings
+RETRIEVAL_K = 3  # Number of most relevant chunks to fetch
